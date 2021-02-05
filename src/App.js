57250206b15c7f1c2d169c40c68e291f30817623
Stefan du Fresne <stefan@sdufresne.info>
2021-02-05 13:45:43 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import './App.scss';
+import React from 'react';
+import { Router, Link } from "@reach/router"
+
+const Home = (props) => {
+  const items = props.checklists.map(checklist => 
+    <li key={checklist.id}><Link to={'/checklist/' + checklist.id}>{checklist.name}</Link></li>
+  );
+
+ return <ul className='App-checklist-list'>{items}</ul>;
+};
+
+const Checklist = (props) => {
+  const checklist = props.checklists.find(c => c.id === props.checklistId);
+
+  const items = checklist.items.map(item => 
+    <li key={item}>{item}</li>
+  );
+
+  return <div>
+    <header>{checklist.name}</header>
+    <ol>{items}</ol>
+  </div>;
+};
+
+class App extends React.Component {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      checklists: [
+        {
+          id: 'before-pushing',
+          name: 'Before you push to Github',
+          items: [
+            'Does ESLint pass?',
+            'Have you covered your new functionality with unit tests?',
+            'How about integration tests?'
+          ]
+        },
+        {
+          id: 'wrist-exercises',
+          name: 'Wrist exercises',
+          items: [
+            '20x rotate CW',
+            '20x rotate CCW',
+            '10x10s ball squeeze',
+            '10x10s band stretch'
+          ]
+        }
+      ],
+    };
+  }
+
+  render() {   
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <Link to='/'>Sanremo</Link>
+        </header>
+        <Router>
+          <Home path='/' checklists={this.state.checklists} />
+          <Checklist path='checklist/:checklistId' checklists={this.state.checklists}/>
+        </Router>
+      </div>
+    );   
+  }
+}
+
+export default App;
