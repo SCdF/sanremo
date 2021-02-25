183b19b37b0ab2727091d1f69fab2d283a7b9156
Stefan du Fresne <stefan@sdufresne.info>
2021-02-25 13:14:14 +0000

    Prod deploy support

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Basic test', () => {
   render(<App />);
   const linkElement = screen.getByText(/Sanremo/i);
   expect(linkElement).toBeInTheDocument();
