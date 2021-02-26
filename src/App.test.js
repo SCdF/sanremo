1d0659a5b3cf341ddb003d66614f4ed3e4600f50
Stefan du Fresne <stefan@sdufresne.info>
2021-02-26 15:50:42 +0000

    dropping context it was a bad idea

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+import db from './db';
+jest.mock(db);
+
+beforeAll(() => {
+  db.mockImplementation(() => {
+    return {
+      find: Promise.resolve({docs: []})
+    }
+  })
+});
+
+beforeEach(() => {
+  db.mockClear();
+});
+
 test('Basic test', () => {
   render(<App />);
   const linkElement = screen.getByText(/Sanremo/i);
