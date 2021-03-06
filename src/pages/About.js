9f09b5840f57d96a21c25e7e2d063050696b6756
Stefan du Fresne <stefan@sdufresne.info>
2021-03-06 14:31:57 +0000

    hopefully debug info

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,22 +1,34 @@
+import { Link } from "@reach/router";
+import { useEffect, useState } from "react";
 import Page from "../components/Page";
 
-function About() {
-  // TODO: either blacklist or whitelist entries for security
-  // TODO: sort
-  const vars = process.env;
+function About(props) {
+  const { db } = props;
 
-  const varRows = Object.keys(vars).map(k => (
-    <tr key={k}><td>{k}</td><td>{vars[k]}</td></tr>
-  ));
+  const [ dbInfo, setDbInfo ] = useState({});
+
+  useEffect(() => db.info().then(info => setDbInfo(info)), [db]);
+
+  const vars = [
+    ['Deployment Type', <b>{process.env.NODE_ENV.toUpperCase()}</b>],
+  ];
+
+  if (process.env.NODE_ENV === 'production') {
+    vars.push([
+      ['Release Version', process.env.REACT_APP_RELEASE_VERSION],
+      ['Release Created At', Date.parse(process.env.REACT_APP_RELEASE_CREATED_AT)],
+      ['Release Description', process.env.REACT_APP_RELEASE_DESCRIPTION],
+      ['Commit', <Link href={`https://github.com/scdf/sanremo/commit/${process.env.REACT_APP_COMMIT}`}>{process.env.REACT_APP_COMMIT}</Link>]
+    ]);
+  }
+
+  vars.push(['Local Database', JSON.stringify(dbInfo)]);
 
   return (
     <Page title="About" back under="about">
       <table>
-        <thead>
-          <tr><th colspn="2">Environment Variables</th></tr>
-        </thead>
         <tbody>
-          {varRows}
+          {vars.map(([k,v], idx) => <tr key={idx}><td>{k}</td><td>{v}</td></tr>)}
         </tbody>
       </table>
     </Page>
