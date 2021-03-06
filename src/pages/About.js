df154d756127d82eac786bf8ce66104dda7d7b9a
Stefan du Fresne <stefan@sdufresne.info>
2021-03-06 12:15:33 +0000

    table!

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,24 @@
 import Page from "../components/Page";
 
 function About() {
+  // TODO: either blacklist or whitelist entries for security
+  // TODO: sort
+  const vars = process.env;
+
+  const varRows = Object.keys(vars).map(k => (
+    <tr key={k}><td>{k}</td><td>{vars[k]}</td></tr>
+  ));
+
   return (
     <Page title="About" back under="about">
-      <blockquote>
-        {JSON.stringify(process.env, null, 2)}
-      </blockquote>
+      <table>
+        <thead>
+          <tr><th colspn="2">Environment Variables</th></tr>
+        </thead>
+        <tbody>
+          {varRows}
+        </tbody>
+      </table>
     </Page>
   );
 }
