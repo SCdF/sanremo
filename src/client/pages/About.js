eb64d19c079bebf0cb3b4e841c4ea054b853608b
Stefan du Fresne <stefan@sdufresne.info>
2021-03-15 13:45:20 +0000

    fixing about page
    By implementing a server lol

--- src/client/pages/About.js
+++ src/client/pages/About.js
@@ -1,18 +1,45 @@
+import axios from 'axios';
+
 import { Link } from "@material-ui/core";
 import { useEffect, useState } from "react";
 import Page from "../components/Page";
+
+function mapProps(parent, info) {
+  return Object.keys(info).sort().map((k) => ([`${parent}.${k}`, info[k]]));
+}
 
 function About(props) {
   const { db } = props;
 
   const [ dbInfo, setDbInfo ] = useState([]);
+  const [ serverInfo, setServerInfo] = useState([]);
 
-  useEffect(() => db.info().then(info => setDbInfo(Object.keys(info).sort().map((k) => ([`db.${k}`, info[k]])))), [db]);
+  useEffect(() => db.info().then(info => setDbInfo(mapProps('db', info))), [db]);
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      axios.get('./api/deployment')
+        .then(response => {
+          const {
+            deploy_created_at: created,
+            deploy_version: deployVersion,
+            release_version: releaseVersion,
+            deploy_commit: hash
+          } = response.data;
+
+          setServerInfo([
+            ['Deployed At', new Date(created).toLocaleString()],
+            ['Deploy Version', deployVersion],
+            ['Release', releaseVersion],
+            ['Build Commit', <Link href={`https://github.com/scdf/sanremo/commit/${hash}`}>{hash}</Link>],
+          ])
+        })
+        .catch(console.error);
+    }
+  });
 
   const vars = [
     ['Deployment Type', <b>{process.env.NODE_ENV.toUpperCase()}</b>],
-    ['Build Date', new Date(process.env.REACT_APP_RELEASE_CREATED_AT).toLocaleString()],
-    ['Build Commit', <Link href={`https://github.com/scdf/sanremo/commit/${process.env.REACT_APP_COMMIT}`}>{process.env.REACT_APP_COMMIT}</Link>],
+    ...serverInfo,
     ...dbInfo
   ];
 
--- src/server/index.js
+++ src/server/index.js
@@ -4,8 +4,26 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../..', 'build')));
 
+app.get('/api/deployment', function (req, res) {
+  if (process.env.NODE_ENV === 'production') {
+    return res.json({
+      deploy_created_at: process.env.HEROKU_RELEASE_CREATED_AT,
+      deploy_version: process.env.HEROKU_RELEASE_VERSION,
+      deploy_commit: process.env.HEROKU_SLUG_COMMIT,
+      release_version: require('../../package.json').version,
+    });
+  }
+
+  return res.json({
+    deploy_created_at: '1970-01-01T12:12:12Z',
+    deploy_version: 'continuous',
+    deploy_commit: 'local HEAD',
+    release_version: require('../../package.json').version,
+  });
+});
+
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', '../..', 'index.html'));
+  res.sendFile(path.join(__dirname, '../..', 'build', 'index.html'));
 });
 
 app.listen(5000);
