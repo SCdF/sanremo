dbaa356c9e49264e40ca4012182336ede81c39d7
Stefan du Fresne <stefan@sdufresne.info>
2021-03-15 14:02:38 +0000

    fixing unit test booting

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,5 @@
+// This file is what create-react-app hooks into for testing so it has to be here
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
