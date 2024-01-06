8f0bc0bc8f2619c1c36e6fd6586c722f8d63a20d
Stefan du Fresne <stefan@sdufresne.info>
2024-01-06 20:49:53 +0000

    unhinged

--- src/client/service-worker.js
+++ src/client/service-worker.js
@@ -14,3 +14,22 @@ async function activate() {
   await Promise.all(keys.map((key) => key !== version && caches.delete(key)));
 }
 addEventListener('activate', (e) => e.waitUntil(activate()));
+
+addEventListener('fetch', async (event) => {
+  const url = event.request.url;
+  console.info(url, 'fetch', { event });
+  const fromCache = await caches.match(event.request);
+  if (fromCache) {
+    console.info(url, 'in cache', { fromCache });
+    return event.respondWith(fromCache);
+  }
+
+  if (event.request.url.endsWith('/')) {
+    const index = await caches.match('/index.html');
+    console.info(url, 'means index', { index });
+    return event.respondWith(index);
+  }
+
+  console.info(url, 'gotta fetch');
+  return event.respondWith(fetch(event.request));
+});
