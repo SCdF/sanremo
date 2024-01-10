7f6f1ff83810916a07054ac747b24a5a10fbb493
Stefan du Fresne <stefan@sdufresne.info>
2024-01-10 20:04:58 +0000

    good god

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-// This entirely exists for jest
-module.exports = {
-  presets: [
-    '@babel/preset-typescript',
-    '@babel/preset-env',
-    ['@babel/preset-react', { runtime: 'automatic' }],
-  ],
-};
